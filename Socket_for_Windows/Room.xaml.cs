b9992bf9e23c73ccf81d50806baa4b0dcb8f8817
jeff29cc91079 <jeff29cc91079@gmail.com>
2021-11-22 13:35:45 +0800

    加入專案檔案。

---
+++ Socket_for_Windows/Room.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Socket_for_Windows
+{
+    /// <summary>
+    /// Room.xaml 的互動邏輯
+    /// </summary>
+    public partial class Room : UserControl
+    {
+        public Room()
+        {
+            InitializeComponent();
+        }
+    }
+}
